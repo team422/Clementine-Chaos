8d4774913cee4db25120d4e13df34215f7a5ea72
Knaoinr <knaoinr@gmail.com>
2020-11-15 17:44:56 -0500

    Main menu

---
+++ Assets/Scripts/MainMenuHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuHandler : MonoBehaviour
+{
+
+    public static String selected = "";
+
+    public static bool isBrownSelected = false;
+    public static bool isYellowSelected = false;
+    public static bool isBlueSelected = false;
+    public static bool isPurpleSelected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
