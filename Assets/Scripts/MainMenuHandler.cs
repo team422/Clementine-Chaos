c783034ee86cf315471038cf0546c08e65e7c0b3
Knaoinr <knaoinr@gmail.com>
2020-11-15 19:20:45 -0500

    switch between scenes

--- Assets/Scripts/MainMenuHandler.cs
+++ Assets/Scripts/MainMenuHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuHandler : MonoBehaviour
 {
@@ -12,15 +13,21 @@ public class MainMenuHandler : MonoBehaviour
     public static bool isBlueSelected = false;
     public static bool isPurpleSelected = false;
 
+    private AsyncOperation gameLoading;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameLoading = SceneManager.LoadSceneAsync("GameScene");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameLoading.isDone) SwitchToGame();
+    }
+
+    void SwitchToGame() {
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("GameScene"));
     }
 }
